c634874d5aa4b92691d4ed899e5207a5fa7595ed
Megan Baker <mebaker@andrew.cmu.edu>
2022-04-19 19:48:32 -0400

    articles branch

---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+class Article < ApplicationRecord
+    has_one :category 
+
+    validates_presence_of :title, :content
+
+    scope :active, 			 -> { where('active = ?', true) }
+  	scope :alphabetical, -> { order('title') }
+end
