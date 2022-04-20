f531094e126db6a2aef5884e645e04dd7c8e40e3
Megan Baker <mebaker@andrew.cmu.edu>
2022-04-19 20:05:23 -0400

    update model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ApplicationRecord
     has_one :category 
 
-    validates_presence_of :title, :content
+    validates_presence_of :title
+    validates_presence_of :content
 
     scope :active, 			 -> { where('active = ?', true) }
   	scope :alphabetical, -> { order('title') }
